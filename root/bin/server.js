7857e145779fbae26ad6a699bbe732b0785e3620
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-05-23 11:01:35 -0700

    commit3

---
+++ root/bin/server.js
@@ -0,0 +1,10 @@
+var debug = require('debug')('my-application');
+var app = require('../index');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+module.exports = server;
--- root/index.js
+++ root/index.js
@@ -5,12 +5,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//spin up server to listen for http requests
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-  //util.format -> %d for number; %s for string
-});
-
 //initialize nexmo client w/ apikey & secret
 const nexmo = new Nexmo({
   apiKey: "02161ced",
