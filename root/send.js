7b6a4635c40bc2689578173708ab6f4f185c7a14
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-05-25 18:08:20 -0700

    commit3

--- root/send.js
+++ root/send.js
@@ -2,8 +2,8 @@ const Nexmo = require('nexmo');
 const prettyjson = require('prettyjson');
 
 const nexmo = new Nexmo({
-  apiKey: '',
-  apiSecret: ''
+  apiKey: 'apiKey',
+  apiSecret: 'apiSecret'
 });
 
 nexmo.message.sendSms('VIRTUALNUMBER', 'TONUMBER', 'TEXT', (err, responseData) => {
