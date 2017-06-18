d776f2145c17207da75396c8abda101455321a09
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-18 15:55:56 -0700

    Added JSON.stringify

--- root/send.js
+++ root/send.js
@@ -9,6 +9,6 @@ nexmo.message.sendSms('VIRTUALNUMBER', 'TONUMBER', 'TEXT', (err, responseData) =
   if (err) {
     console.log(err);
   } else {
-    console.dir(responseData);
+    console.log(JSON.stringify(responseData, null, 2));  
   }
 });
